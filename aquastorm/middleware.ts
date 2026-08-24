9310385c98fd390d37355a4d14f3ab225c468daf
sparkthatstar <sparkthatstar@gmail.com>
2026-08-24 18:18:06 -0500

    Redirect inactive users to home instead of dashboard

--- aquastorm/middleware.ts
+++ aquastorm/middleware.ts
@@ -59,7 +59,7 @@ export async function middleware(request: NextRequest) {
   }
 
   if (!canAccess(profile.role, pathname)) {
-    return NextResponse.redirect(new URL('/dashboard', request.url))
+    return NextResponse.redirect(new URL('/', request.url))
   }
 
   return response
